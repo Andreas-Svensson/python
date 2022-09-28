eae1c72cbbe4f57f3c721a113fa6c9f49c34e7ac
Andreas Svensson <andreas.svensson@iths.se>
2022-09-28 14:06:18 +0200

    fixed moving shape within screen boundaries

--- Explorations/Tetris/learning_pygame.py
+++ Explorations/Tetris/learning_pygame.py
@@ -6,7 +6,7 @@ pygame.init()
 
 size = width, height = 300, 600 # window size
 black = 0, 0, 0 # background color
-gray = 100, 100, 100
+gray = 100, 100, 100 # shape color
 
 screen = pygame.display.set_mode(size) # set screen to size
 
@@ -15,10 +15,13 @@ screen = pygame.display.set_mode(size) # set screen to size
 # ball = pygame.image.load("c:/GitHub/Python-Andreas-Svensson/Explorations/Tetris/assets/intro_ball.gif")
 # ballrect = ball.get_rect()
 
-square = pygame.Rect(0, 0, 30, 30) # NOTE: Test creation of rect
+shape_x = 0
+shape_y = 0
+shape_width = 30
+shape_height = 30
 
 # ----- position and velocity variables -----
-vel = 15 # change factor of velocity for input move commands
+vel = 30 # change factor of velocity for input move commands
 
 while 1:
     pygame.time.delay(100) # set a delay of n ms
@@ -29,16 +32,25 @@ while 1:
         if event.type == pygame.QUIT: sys.exit() # exit if exit
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and square.left - vel >= 0:
-                square = square.move(-vel, 0)
-            if event.key == pygame.K_RIGHT and square.right + vel < 300:
-                square = square.move(vel, 0)
-            if event.key == pygame.K_UP and square.top - vel >= 0:
-                square = square.move(0, -vel)
-            if event.key == pygame.K_DOWN and square.bottom < height - vel:
-                square = square.move(0, vel)
+            if event.key == pygame.K_LEFT and shape_x - vel >= 0:
+                shape_x -= vel
+
+            if event.key == pygame.K_RIGHT and shape_x + vel < width:
+                shape_x += vel
+
+            if event.key == pygame.K_UP and shape_y - vel >= 0:
+                shape_y -= vel
+
+            if event.key == pygame.K_DOWN and shape_y + vel < height:
+                shape_y += vel
 
     # ----- overwrite old screen with black, draw new screen, and show (flip) it -----
     screen.fill(black)
-    screen.blit(screen, pygame.draw.rect(screen, gray, square)) # NOTE: Drawing test rect on screen
+    
+    for i in range(600):
+        screen.set_at((150, i), gray)
+        screen.set_at((i, 300), gray)
+
+    #screen.blit(screen, pygame.draw.rect(screen, gray, square)) # NOTE: Drawing test rect on screen
+    pygame.draw.rect(screen, gray, (shape_x, shape_y, shape_width, shape_height))
     pygame.display.flip()
