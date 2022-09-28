6912827ad6b0ec5190c82082074defda0c78098f
Andreas Svensson <andreas.svensson@iths.se>
2022-09-28 12:02:07 +0200

    changed borrowed code to simple tetris board

--- Explorations/Tetris/learning_pygame.py
+++ Explorations/Tetris/learning_pygame.py
@@ -1,32 +1,44 @@
 """Code from introduction page on pygame"""
 
 import sys, pygame
+import numpy as np
 pygame.init()
 
-size = width, height = 720, 480
-speed = [4, 3]
-black = 0, 0, 0
+size = width, height = 300, 600 # window size
+black = 0, 0, 0 # background color
+gray = 100, 100, 100
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size) # set screen to size
 
+# ----- read in image file -----
 # NOTE: in future: use os.path.join to build path and concatenate with relative file path
+# ball = pygame.image.load("c:/GitHub/Python-Andreas-Svensson/Explorations/Tetris/assets/intro_ball.gif")
+# ballrect = ball.get_rect()
 
-ball = pygame.image.load("c:/GitHub/Python-Andreas-Svensson/Explorations/Tetris/assets/intro_ball.gif")
-ballrect = ball.get_rect()
+square = pygame.Rect(0, 0, 30, 30) # NOTE: Test creation of rect
+
+# ----- position and velocity variables -----
+vel = 15 # change factor of velocity for input move commands
 
 while 1:
-    # create a delay of 10 ms
-    pygame.time.delay(10)
+    pygame.time.delay(100) # set a delay of n ms
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+    # ----- events -----
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT: sys.exit() # exit if exit
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and square.left - vel >= 0:
+                square = square.move(-vel, 0)
+            if event.key == pygame.K_RIGHT and square.right + vel < 300:
+                square = square.move(vel, 0)
+            if event.key == pygame.K_UP and square.top - vel >= 0:
+                square = square.move(0, -vel)
+            if event.key == pygame.K_DOWN and square.bottom < height - vel:
+                square = square.move(0, vel)
 
+    # ----- overwrite old screen with black, draw new screen, and show (flip) it -----
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(screen, pygame.draw.rect(screen, gray, square)) # NOTE: Drawing test rect on screen
     pygame.display.flip()
