040a215230eaf17ee38373e1fd892042a20fcd3e
Andreas Svensson <andreas.svensson@iths.se>
2022-09-29 20:12:14 +0200

    added basic shape arrays

--- Explorations/Tetris/tetrominoes.py
+++ Explorations/Tetris/tetrominoes.py
@@ -1,5 +1,24 @@
-class Shape:
-    def __init__(self, name) -> None:
-        self.name = name
+import numpy as np
 
-T = Shape("T-Shape")
+class Shape:
+    def __init__(self, shape) -> None:
+        self.shape = shape
+    
+    def __repr__(self):
+        return f"shape:\n{self.shape}"
+
+I = Shape(np.array(([0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]))) # I-shape
+O = Shape(np.array(([0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]))) # O-shape
+T = Shape(np.array(([0, 1, 0], [1, 1, 1], [0, 0, 0]))) # T-shape
+J = Shape(np.array(([1, 0, 0], [1, 1, 1], [0, 0, 0]))) # J-shape
+L = Shape(np.array(([0, 0, 1], [1, 1, 1], [0, 0, 0]))) # L-shape
+S = Shape(np.array(([0, 1, 1], [1, 1, 0], [0, 0, 0]))) # S-shape
+Z = Shape(np.array(([1, 1, 0], [0, 1, 1], [0, 0, 0]))) # Z-shape
+
+print(I)
+print(O)
+print(T)
+print(J)
+print(L)
+print(S)
+print(Z)
