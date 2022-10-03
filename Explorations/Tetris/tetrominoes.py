aac68b207a4034c60b053f636bb30bbaf7942879
Andreas Svensson <andreas.svensson@iths.se>
2022-10-03 07:48:44 +0200

    added color

--- Explorations/Tetris/tetrominoes.py
+++ Explorations/Tetris/tetrominoes.py
@@ -1,32 +1,33 @@
 import numpy as np
 
 class Shape:
-    def __init__(self, shape) -> None:
+    def __init__(self, shape, color: tuple) -> None:
         self.shape = shape
+        self.color = color
     
     def __repr__(self):
-        return f"shape:\n{self.shape}"
+        return f"shape({self.shape=}, {self.color=}"
 
-I = Shape(np.array(([0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]))) # I-shape
-O = Shape(np.array(([0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]))) # O-shape
-T = Shape(np.array(([0, 1, 0], [1, 1, 1], [0, 0, 0]))) # T-shape
-J = Shape(np.array(([1, 0, 0], [1, 1, 1], [0, 0, 0]))) # J-shape
-L = Shape(np.array(([0, 0, 1], [1, 1, 1], [0, 0, 0]))) # L-shape
-S = Shape(np.array(([0, 1, 1], [1, 1, 0], [0, 0, 0]))) # S-shape
-Z = Shape(np.array(([1, 1, 0], [0, 1, 1], [0, 0, 0]))) # Z-shape
+I = Shape(np.array(([0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0])), color = (0, 255, 255)) # I-shape
+O = Shape(np.array(([0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0])), color = (255, 255, 0)) # O-shape
+T = Shape(np.array(([0, 1, 0], [1, 1, 1], [0, 0, 0])), color = (153, 0, 255)) # T-shape
+J = Shape(np.array(([1, 0, 0], [1, 1, 1], [0, 0, 0])), color = (0, 0, 255)) # J-shape
+L = Shape(np.array(([0, 0, 1], [1, 1, 1], [0, 0, 0])), color = (255, 170, 0)) # L-shape
+S = Shape(np.array(([0, 1, 1], [1, 1, 0], [0, 0, 0])), color = (0, 255, 0)) # S-shape
+Z = Shape(np.array(([1, 1, 0], [0, 1, 1], [0, 0, 0])), color = (255, 0, 0)) # Z-shape
 
-print("I-" + str(I))
-print("O-" + str(O))
-print("T-" + str(T))
-print("J-" + str(J))
-print("L-" + str(L))
-print("S-" + str(S))
-print("Z-" + str(Z))
+# print("I-" + str(I))
+# print("O-" + str(O))
+# print("T-" + str(T))
+# print("J-" + str(J))
+# print("L-" + str(L))
+# print("S-" + str(S))
+# print("Z-" + str(Z))
 
 for i in I.shape:
-    print(i)
+    print(i) # all indices in shape
 
-print(I.shape[I.shape>0])
+print(I.shape[I.shape>0]) # non-empty indices in shape
 
-x, y = np.where(I.shape == 1)
+x, y = np.where(I.shape == 1) # index of non-empty indices in shape
 print(x, y)
