3b0f88c509d998861114581aa691b55a3c909f1c
Andreas Svensson <andreas.svensson@iths.se>
2022-10-04 09:05:28 +0200

    updated comments, added todos

--- Explorations/Tetris/tetrominoes.py
+++ Explorations/Tetris/tetrominoes.py
@@ -1,39 +1,36 @@
 import numpy as np
 
 class Shape:
-    def __init__(self, shape, color: tuple) -> None:
+    def __init__(self, shape, color: tuple) -> None: # TODO type hinting to np.array?
         self.shape = shape
         self.color = color
-    
-    def __repr__(self):
-        return f"shape({self.shape=}, {self.color=}"
+
+    # TODO properties
     
     def rotate_clockwise(self):
+        # TODO check validity of move
         self.shape = np.rot90(self.shape, 3) # rotates 90* counter clockwise 3 times
 
     def rotate_counter_clockwise(self):
+        # TODO check validity of move
         self.shape = np.rot90(self.shape)
 
-I = Shape(np.array(([0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0])), color = (0, 255, 255)) # I-shape
-O = Shape(np.array(([0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0])), color = (255, 255, 0)) # O-shape
-T = Shape(np.array(([0, 1, 0], [1, 1, 1], [0, 0, 0])), color = (153, 0, 255)) # T-shape
-J = Shape(np.array(([1, 0, 0], [1, 1, 1], [0, 0, 0])), color = (0, 0, 255)) # J-shape
-L = Shape(np.array(([0, 0, 1], [1, 1, 1], [0, 0, 0])), color = (255, 170, 0)) # L-shape
-S = Shape(np.array(([0, 1, 1], [1, 1, 0], [0, 0, 0])), color = (0, 255, 0)) # S-shape
-Z = Shape(np.array(([1, 1, 0], [0, 1, 1], [0, 0, 0])), color = (255, 0, 0)) # Z-shape
+    # TODO draw method here instead of in game
+    
+    def __repr__(self):
+        return f"Shape({self.shape=}, {self.color=})"
 
-# print("I-" + str(I))
-# print("O-" + str(O))
-# print("T-" + str(T))
-# print("J-" + str(J))
-# print("L-" + str(L))
-# print("S-" + str(S))
-# print("Z-" + str(Z))
+# TODO class Board
+    # TODO __init__
 
-for i in I.shape:
-    print(i) # all indices in shape
+    # TODO properties
 
-print(I.shape[I.shape>0]) # non-empty indices in shape
+    # TODO draw board
 
-x, y = np.where(I.shape == 1) # index of non-empty indices in shape
-print(x, y)
+# for i in I.shape:
+#     print(i) # all indices in shape
+
+# print(I.shape[I.shape>0]) # non-empty indices in shape
+
+# x, y = np.where(I.shape == 1) # index of non-empty indices in shape
+# print(x, y)
